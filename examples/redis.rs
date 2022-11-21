5daebb20a2654b47b1a00fb04d0d91a559fa856c
romnnn <roman.dahm@gmx.de>
2022-11-21 01:14:26 +0100

    update to new feature combinations api

--- examples/redis.rs
+++ examples/redis.rs
@@ -1,7 +1,7 @@
 use color_eyre::eyre;
 
 #[tokio::main]
-fn main() -> eyre::Result<()> {
+async fn main() -> eyre::Result<()> {
     // req := testcontainers.ContainerRequest{
     // Image:        "nginx",
     // ExposedPorts: []string{"80/tcp"},
